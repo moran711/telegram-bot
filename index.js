a169d638909416f3cc6e0b41e685f6f16811df77
Roman <romaniv788@gmail.com>
2021-03-24 19:42:46 +0200

    add messages for empty data

--- index.js
+++ index.js
@@ -11,12 +11,21 @@ connectDB();
 const token = process.env.BOT_TOKEN;
 
 const port = process.env.PORT || 8443;
-const host = process.env.NODE_ENV === 'production' ? process.env.HOST : 'http://localhost';
+const host =
+  process.env.NODE_ENV === "production" ? process.env.HOST : "http://localhost";
 
-const bot = new TelegramBot(token, { polling: true, webHook: {port: port, host: host }});
+const botOptions = {
+  polling: process.env.NODE_ENV === "production",
+  webHook: {
+    port: process.env.NODE_ENV === "production" ? port : null,
+    host: process.env.NODE_ENV === "production" ? host : null,
+  },
+};
 
-if (process.env.NODE_ENV === 'production')
-bot.setWebHook(`${host}:${port}/bot${token}`);
+const bot = new TelegramBot(token, botOptions);
+
+if (process.env.NODE_ENV === "production")
+  bot.setWebHook(`${host}:${port}/bot${token}`);
 
 bot.on("callback_query", async (query) => {
   const chatId = query.message.chat.id;
@@ -26,33 +35,39 @@ bot.on("callback_query", async (query) => {
     const cathedras = await cathedraController.getAllCathedras({
       institute: selectedItem,
     });
-    bot.sendMessage(chatId, "Вибери із списку свою кафедру", {
-      reply_markup: {
-        inline_keyboard: formatDataForKeyboard(cathedras),
-      },
-    });
+    cathedras.length
+      ? bot.sendMessage(chatId, "Вибери із списку свою кафедру", {
+          reply_markup: {
+            inline_keyboard: formatDataForKeyboard(cathedras),
+          },
+        })
+      : bot.sendMessage(chatId, "Для вибраного інституту поки немає кафедри");
   } else if (await cathedraController.getCathedraById(selectedItem)) {
     const groups = await groupController.getAllGroups({
       cathedra: selectedItem,
     });
-    bot.sendMessage(chatId, "Вибери із списку свою групу", {
-      reply_markup: {
-        inline_keyboard: formatDataForKeyboard(groups),
-      },
-    });
+    groups.length
+      ? bot.sendMessage(chatId, "Вибери із списку свою групу", {
+          reply_markup: {
+            inline_keyboard: formatDataForKeyboard(groups),
+          },
+        })
+      : bot.sendMessage(chatId, "Для вибраної кафедри поки немає групи");
   } else if (await groupController.getGroupById(selectedItem)) {
-    bot.sendMessage(chatId, "Розкладу для групи поки немає")
+    bot.sendMessage(chatId, "Розкладу для групи поки немає");
   }
 });
 
 bot.onText(/\/get_scedule/, async (msg) => {
   const chatId = msg.chat.id;
   const institutes = await instituteController.getAllInstitutes();
-  bot.sendMessage(chatId, "Вибери із списку свій інститут", {
-    reply_markup: {
-      inline_keyboard: formatDataForKeyboard(institutes),
-    },
-  });
+  institutes.length
+    ? bot.sendMessage(chatId, "Вибери із списку свій інститут", {
+        reply_markup: {
+          inline_keyboard: formatDataForKeyboard(institutes),
+        },
+      })
+    : bot.sendMessage(chatId, "Інститутів поки немає");
 });
 
 bot.on("polling_error", (err) => console.log(err));
