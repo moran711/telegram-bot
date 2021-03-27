e5114bfe02b2aada2040d3253a7e7660beb14f0a
Roman <romaniv788@gmail.com>
2021-03-27 16:55:23 +0200

    fix prod

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const host =
 const botOptions = {
   polling: process.env.NODE_ENV !== "production"
 };
-console.log(`${host}:${port}/bot${token}`);
+console.log(`${host.substring(0, host.length - 1)}:${port}/bot${token}`);
 const bot = new TelegramBot(token, botOptions);
 
 if (process.env.NODE_ENV === "production")
